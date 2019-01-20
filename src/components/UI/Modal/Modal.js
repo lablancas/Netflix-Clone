706fac90f370dd86477c7a2da3cef442921c73a5
Lucas Blancas <lablancas@gmail.com>
2019-01-20 15:15:53 -0600

    remove background image in modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,7 +8,6 @@ class Modal extends Component {
    render() {
       const backgroundStyle = {
          backgroundSize: "cover",
-         backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.props.movie.backdrop_path || this.props.movie.poster_path})`,
       }
       return (
          <Aux>
