92e018bb4521e824ed52042b8e1e2daafece6b6b
Lucas Blancas <lablancas@gmail.com>
2019-01-20 15:25:33 -0600

    remove all category rows

--- src/containers/MovieGenreRow/MovieGenreRow.js
+++ src/containers/MovieGenreRow/MovieGenreRow.js
@@ -8,30 +8,12 @@ class MovieGenreRow extends Component {
 
   /** Hold each genre movie row in an array */
   state = {
-    trendingMovieRow: [],
-    netflixOriginalsRow: [],
-    topRatedRow: [],
-    comedyMovieRow: [],
-    horrorMovieRow: [],
-    actionMovieRow: [],
-    animatedMovieRow: [],
-    documentaryRow: [],
-    romanceMovieRow: [],
     movieOverview: {},
     toggleModal: false,
   }
 
   /** Make all API calls as soon as our MovieGenreRow component mounts. */
   componentWillMount() {
-    this.getTrending(); 
-    this.getTopRated(); 
-    this.getComedyMovies(); 
-    this.getActionMovies(); 
-    this.getHorrorMovies(); 
-    this.getNetflixMovies(); 
-    this.getRomanceMovies(); 
-    this.getDocumentaries(); 
-    this.getAnimatedMovies(); 
   }
   
   /** 
@@ -77,189 +59,10 @@ class MovieGenreRow extends Component {
        
   }
 
-  /**
-   * Send request for movies that are popular right now
-   */
-  getTrending = () => {
-    const url = "/trending/all/week?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&language=en-US"; 
-   
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-        
-        this.setState({ trendingMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  /**
-   * Send request for movies that are top rated 
-   */
-  getTopRated = () => {
-    const url = "/movie/top_rated?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&language=en-US"; 
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ topRatedRow: movieRows }); 
-      }).catch(error => {
-        console.log(error); 
-      })
-  }
-
-  /**
-   * Get Netflix originals 
-   */
-  getNetflixMovies = () => {
-    const url = "/discover/tv?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_networks=213";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url); 
-
-        this.setState({ netflixOriginalsRow: movieRows }); 
-      })
-      .catch(error => {
-        console.log(error); 
-      })
-  }
-
-  getActionMovies = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=28";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ actionMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  getComedyMovies = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=35";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ comedyMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  getHorrorMovies = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=27";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ horrorMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  getRomanceMovies = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=10749";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ romanceMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  getAnimatedMovies = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=16";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ animatedMovieRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
-  getDocumentaries = () => {
-    const url = "/discover/movie?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0&with_genres=99";
-
-    axios.get(url)
-      .then(res => {
-        const movieRows = this.getMovieRows(res, url);
-
-        this.setState({ documentaryRow: movieRows });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
    render() {
 
       return (  
         <div className="movieShowcase">
-          <h1 className="movieShowcase__heading">NETFLIX ORIGINALS</h1>
-          <div className="movieShowcase__container">
-            {this.state.netflixOriginalsRow}
-          </div>
-          
-          <h1 className="movieShowcase__heading">Trending Now</h1>
-          <div className="movieShowcase__container">
-              {this.state.trendingMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Top Rated</h1>
-          <div className="movieShowcase__container">
-              {this.state.topRatedRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Action Movies</h1>
-          <div className="movieShowcase__container">
-              {this.state.actionMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Comedy Movies</h1>
-          <div className="movieShowcase__container">
-              {this.state.comedyMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Horror Movies</h1>
-          <div className="movieShowcase__container">
-              {this.state.horrorMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Romance Movies</h1>
-          <div className="movieShowcase__container">
-              {this.state.romanceMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Animated Films</h1>
-          <div className="movieShowcase__container">
-              {this.state.animatedMovieRow}
-          </div>
-
-          <h1 className="movieShowcase__heading">Documentaries</h1>
-          <div className="movieShowcase__container">
-              {this.state.documentaryRow}
-          </div>
-        
           <Modal show={this.state.toggleModal} modalClosed={this.closeModal} movie={this.state.movieOverview}>
             <MovieDetails movie={this.state.movieOverview}/>
           </Modal>
